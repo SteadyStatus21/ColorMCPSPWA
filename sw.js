6acb93861f3d7e93292518084929306cb362602e
SteadyStatus21 <73841304+SteadyStatus21@users.noreply.github.com>
2023-03-06 13:56:30 +0000

    baller

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('sw-cache').then(function(cache) {
-            return cache.add('index.html');
-        })
-    );
+const CACHE_NAME = 'color-mcps-cache-v1';
+const urlsToCache = [
+  '/',
+  '/ColorMCPSPWA/manifest.json',
+  '/sw.js',
+  'https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js',
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => {
+        // Return offline message
+        return new Response('You are offline. Please check your internet connection and try again.', {
+          headers: { 'Content-Type': 'text/plain' }
+        });
+      })
+  );
 });
